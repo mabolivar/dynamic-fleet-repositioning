da8d654e97e2a4d3187631e3a8a3d7be6cf6bb4e
Manuel A. Bolívar <ma.bolivar643@gmail.com>
2022-07-03 20:48:02 +0200

    [Fix] Compute cost based on actions (#4)
    Fix bug where cost was computed based on courier's current location.

--- source/policies/policy.py
+++ source/policies/policy.py
@@ -22,12 +22,13 @@ class Policy:
         scenario_actions = []
         scenario_costs = []
         for epoch in range(scenario.epochs):
-            print(f"Scenario: {scenario.index} - Epoch: {epoch}")
             actions = self.take_action(state)
             cost, action_evaluation, state = state.update(scenario, actions)
             scenario_actions.append(action_evaluation)
             scenario_costs.append(cost)
+            print(f"Scenario: {scenario.index} - Epoch: {epoch} - Cost: {cost}")
         execution_secs = time.time() - now
+        print(f"Scenario: {scenario.index} - Execution time: {execution_secs} - Cost: {np.sum(scenario_costs)}")
         return {
             'cost': sum(scenario_costs),
             'actions': scenario_actions,
