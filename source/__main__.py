e836aa495dcbae210b59df22909978d6ee069384
Manuel A. Bolívar <ma.bolivar643@gmail.com>
2022-07-06 11:25:12 +0200

    [Policy] Add Value function approximation policy (#6)
    Add first version of a Value Function Approximation policy

--- source/__main__.py
+++ source/__main__.py
@@ -2,10 +2,11 @@ from source.scenario import Scenario
 from source.utils import load_data, split_data
 from source.policies.last_nearest_order import LastNearestOrder
 from source.policies.do_nothing import DoNothing
+from source.policies.vfa import VFA
 
 PARAMS = {
     'input_data_path': "./data/robotex5.csv",
-    'policies': ['do_nothing', 'last_nearest_order'],
+    'policies': ['do_nothing', 'vfa', 'last_nearest_order'],
     'minutes_bucket_size': 10,
     'courier_km_per_minute': 1.0,
 }
@@ -16,6 +17,8 @@ def get_policy(name):
         return LastNearestOrder
     elif name == 'do_nothing':
         return DoNothing
+    elif name == 'vfa':
+        return VFA
     else:
         raise ValueError('Unknown policy: {}'.format(name))
 
@@ -23,9 +26,9 @@ def get_policy(name):
 if __name__ == '__main__':
     data = load_data(PARAMS['input_data_path'])
     train, test = split_data(data)
-    # train_scenarios = Scenario.generate_scenarios(
-    #     label="train", data=train, minutes_bucket_size=PARAMS['minutes_bucket_size']
-    # )
+    train_scenarios = Scenario.generate_scenarios(
+        label="train", data=train, minutes_bucket_size=PARAMS['minutes_bucket_size']
+    )
     test_scenarios = Scenario.generate_scenarios(
         label="test", data=test, minutes_bucket_size=PARAMS['minutes_bucket_size']
     )
@@ -33,6 +36,19 @@ if __name__ == '__main__':
     policies_performance = [("policy", "scenario", "reward", "perfect_reward", "execution_secs")]
     for policy_name in PARAMS.get('policies', []):
         policy = get_policy(policy_name)(**PARAMS)
+        sum_cost, sum_gap = 0, 0
+        num_scenarios = len(train_scenarios)
+        for scenario in train_scenarios:
+            solution = policy.train(scenario)
+            sum_cost += solution['cost']
+            # sum_gap += solution['gap']
+            policies_performance.append(
+                (policy_name, scenario.label, solution['cost'], scenario.perfect_cost, solution['execution_secs'])
+            )
+
+        print(
+            f"Train -> Policy: {policy_name} | Avg. reward: {sum_cost / num_scenarios} | gap: {(sum_gap / num_scenarios * 100):.1f}%")
+
         sum_cost, sum_gap = 0, 0
         num_scenarios = len(test_scenarios)
         for scenario in test_scenarios:
@@ -44,4 +60,4 @@ if __name__ == '__main__':
             )
 
         print(
-            f"Policy: {policy_name} | Avg. reward: {sum_cost / num_scenarios} | gap: {(sum_gap / num_scenarios * 100):.1f}%")
+            f"Test -> Policy: {policy_name} | Avg. reward: {sum_cost / num_scenarios} | gap: {(sum_gap / num_scenarios * 100):.1f}%")
---
+++ source/policies/vfa.py
@@ -0,0 +1,85 @@
+from itertools import product
+from collections import defaultdict
+from source.state import State
+from source.policies.policy import Policy
+from source.utils import haversine_distance
+
+POLICY_NAME = 'VFA'
+
+
+def find_closest_order(couriers, orders):
+    order_cords = [[order['lat'], order['lng']] for order in orders]
+    courier_cords = [[courier['lat'], courier['lng']] for courier in couriers]
+    order_exploited_cords, courier_exploited_cords = list(zip(*product(order_cords, courier_cords)))
+    distance_array = haversine_distance(*zip(*order_exploited_cords), *zip(*courier_exploited_cords))
+    nearest_order = dict()
+    for j, courier in enumerate(couriers):
+        nearest_order[j] = {'order_id': None, 'distance': float('inf'), 'lat': None, 'lng': None}
+        for i, order in enumerate(orders):
+            d = distance_array[(i * len(couriers) + j)]
+            if d < nearest_order[j]['distance']:
+                nearest_order[(courier['lat'], courier['lng'])] = {
+                    'order_id': i,
+                    'distance': d,
+                    'lat': order['lat'],
+                    'lng': order['lng'],
+                }
+    return nearest_order
+
+
+class VFA(Policy):
+    def __init__(self, **kwargs):
+        super(VFA, self).__init__(
+            name=POLICY_NAME,
+            courier_km_per_minute=kwargs.get('courier_km_per_minute', 1.0),
+            minutes_bucket_size=kwargs.get('minutes_bucket_size', 10)
+        )
+        epoch_in_minutes = range(0, 24 * 60, self.minutes_bucket_size)
+        self.V = {t: defaultdict() for t, _ in enumerate(epoch_in_minutes)}
+        self.n = 1   # Sample path
+        self.epoch = None
+
+    def take_action(self, state: State):
+        orders = state.orders
+        couriers = state.couriers
+        prev_actions = state.prev_actions
+        all_locations = state.locations
+        self.epoch = state.epoch
+        actions = self.compute_actions(couriers)
+
+        if state.epoch > 0 and orders:
+            closest_order_per_location = find_closest_order(all_locations, orders)
+
+            self.update_value_estimates(
+                prev_epoch=state.epoch - 1,
+                nearest_orders=closest_order_per_location
+            )
+        return actions
+
+    def compute_actions(self, couriers):
+        actions = {}
+        for ix, courier in enumerate(couriers):
+            move_lat, move_lng = self.compute_movement_location(courier['lat'], courier['lng'])
+            actions[ix] = {'lat': move_lat, 'lng': move_lng}
+        return actions
+
+    def update_value_estimates(self, prev_epoch: int, nearest_orders: dict):
+        alpha = 0.2 #* (1 - (self.num_iteration + 1) / self.total_iterations)
+        # delta = ceil(10 * (1 - (self.num_iteration + 1) / self.total_iterations))
+        if prev_epoch < 0:
+            return
+        # Value function update algorithm
+        for location in nearest_orders:
+            sampled_value = nearest_orders[location]['distance']
+            prev_value = self.V[prev_epoch].get(location, sampled_value)
+            self.V[prev_epoch][location] = (1-alpha) * prev_value + alpha * sampled_value
+
+    def compute_movement_location(self, start_lat, start_lng):
+        movements = self.get_neighbours(start_lat, start_lng, precision=self.precision)
+        d = float('inf')
+        best_move = (start_lat, start_lng)
+        for i, move in enumerate(movements):
+            if self.V[self.epoch].get(move, d) < d:
+                best_move = move
+                d = self.V[self.epoch][move]
+        return best_move
